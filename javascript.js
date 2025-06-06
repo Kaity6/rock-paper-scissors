78dcdeb29a7d1c0144217a4f67d40467cde2488b
kaity6 <202271975+Kaity6@users.noreply.github.com>
2025-06-06 16:36:24 +0000

    finally figured out playROund function

--- javascript.js
+++ javascript.js
@@ -20,16 +20,21 @@ console.log(getPlayerChoice());
 let humanScore = 0, computerScore = 0;
 
 function playRound(humanSelection,computerSelection) {
-    let humanSelection = getPlayerChoice();
-    let computerSelection = getComputerChoice();
         if (humanSelection == computerSelection) {
             console.log(`It's a draw, both of you picked ${humanSelection}`)
         }
-        else {console.log("meow")}
+        else if (humanSelection == "rock" && computerSelection == "scissors" || humanSelection == "paper" && computerSelection == "rock" || humanSelection == "scissors" && computerSelection == "paper") {
+            console.log(`You won! ${humanSelection} beats ${computerSelection}.`)
+            humanScore ++
+        }
+        else {
+            console.log(`You lost! ${computerSelection} beats ${humanSelection}, skill issue tbh.`)
+            computerScore ++
+        }
 }
+let humanSelection = getPlayerChoice();
+let computerSelection = getComputerChoice();
+console.log(humanSelection);
+console.log(computerSelection);
 playRound(humanSelection,computerSelection);
 
-// humanSelection = getPlayerChoice();
-// computerSelection = getComputerChoice();
-// console.log(humanSelection);
-// console.log(computerSelection);
